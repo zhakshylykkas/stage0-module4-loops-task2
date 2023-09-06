edb417fa62026a86a7101daa692885eb29ce54e3
Zhakshylyk Kasymbekov <jakkasymbekov@gmail.com>
2023-09-06 22:15:08 +0600

    Changes committed to Multiplication task after the second testing

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -3,13 +3,15 @@ package school.mjc.stage0.loops.task2;
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
         int i = 0;
-        if (multiplyByAndToInclusive < 0) {
+        if (multiplyByAndToInclusive == 0) {
+
+        } else if (multiplyByAndToInclusive < 0) {
             while (i >= multiplyByAndToInclusive) {
                 System.out.println(i * multiplyByAndToInclusive * (-1));
                 i--;
             }
         } else {
-            while (i < multiplyByAndToInclusive) {
+            while (i <= multiplyByAndToInclusive) {
                 System.out.println(i * multiplyByAndToInclusive);
                 i++;
             }
